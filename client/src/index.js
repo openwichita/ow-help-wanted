324be53cfac839e0b1140c42c66db8c13f2559a0
Seth Etter <sethetter@gmail.com>
2018-06-23 07:58:00 -0500

    Relaunched with updated lambda URL

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
 
-fetch('https://7kwo0tuep0.execute-api.us-east-1.amazonaws.com/dev/github/data', {
+fetch('https://nv3j6cbum6.execute-api.us-east-1.amazonaws.com/dev/github/data', {
   headers: new Headers({ 'content-type': 'application/json' })
 }).then(response => response.json()).then(({ data }) => {
   ReactDOM.render(<App data={data} />, document.getElementById('root'))
